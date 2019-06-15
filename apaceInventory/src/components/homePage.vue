1e055915000be593b86944d451face80bc1328ae
rhish <rhish9h@gmail.com>
2019-06-15 14:08:59 +0530

    created homePage, added appHeader

---
+++ apaceInventory/src/components/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <appHeader/>
+    </div>
+</template>
+
+<script>
+import appHeader from './appHeader.vue'
+export default {
+  name: 'homePage',
+  components: {
+    appHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
