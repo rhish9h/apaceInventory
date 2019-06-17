3af69638fb7ec9d6b3277770e149ceb296dfa868
rhish <rhish9h@gmail.com>
2019-06-17 18:59:15 +0530

    added basic static side menu

---
+++ apaceInventory/src/components/sideMenu.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sideContainer">
+        <div id="item1">item 1</div>
+        <div id="item2">item 2</div>
+        <div id="item3">item 3</div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    .sideContainer {
+        background-color: #58f1d7a8;
+        width: 15%;
+        height: stretch;
+    }
+    .sideContainer > div {
+        padding: 1em;
+        background-color:  #09f3ccbe;
+        position: relative;
+    }
+</style>
